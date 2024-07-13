6121775498fa6fece42f53f2c3b26021c262cf22
yeonduing <yeonduing@gmail.com>
2024-07-13 19:29:08 +0900

    add RequestRetrier

---
+++ Kustaurant/Kustaurant/Network/RequestRetrier.swift
@@ -0,0 +1,12 @@
+//
+//  RequestRetrier.swift
+//  Kustaurant
+//
+//  Created by 류연수 on 7/13/24.
+//
+
+import Foundation
+
+public protocol RequestRetrier {
+    func shouldRetry(_ request: URLRequest, with error: Error, attempt: Int) -> Bool
+}
