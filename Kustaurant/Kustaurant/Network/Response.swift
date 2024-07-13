a35caca874a30f8d37463a131cc02c18672780ae
yeonduing <yeonduing@gmail.com>
2024-07-13 19:29:41 +0900

    add Response

---
+++ Kustaurant/Kustaurant/Network/Response.swift
@@ -0,0 +1,26 @@
+//
+//  Response.swift
+//  Kustaurant
+//
+//  Created by 류연수 on 7/13/24.
+//
+
+import Foundation
+
+public class Response {
+    public let data: Data?
+    public let error: NetworkError?
+    
+    init(data: Data?, error: NetworkError?) {
+        self.data = data
+        self.error = error
+    }
+}
+
+extension Response {
+    
+    public func decode<T: Decodable>() -> T? {
+        guard let data else { return nil }
+        return try? JSONDecoder().decode(T.self, from: data)
+    }
+}
