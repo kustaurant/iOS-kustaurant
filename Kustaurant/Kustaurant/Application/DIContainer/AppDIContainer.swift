e8405e140172f3c9ab475b7b3284e4cfbda5e106
Woojin Song <woojin1900@gmail.com>
2024-07-16 16:38:59 +0900

    add AppDIContainer

---
+++ Kustaurant/Kustaurant/Application/DIContainer/AppDIContainer.swift
@@ -0,0 +1,13 @@
+//
+//  AppDIContainer.swift
+//  Kustaurant
+//
+//  Created by 송우진 on 7/16/24.
+//
+
+import Foundation
+
+/// 앱의 전체적인 의존성 주입을 관리하는 Container입니다.
+final class AppDIContainer {
+    lazy var appConfiguration = AppConfiguration()
+}
