d1c07a7c9d20a8d7a39aa786d20b7f1f1db5d59b
Woojin Song <woojin1900@gmail.com>
2024-07-23 20:36:16 +0900

    add Location enum

---
+++ Kustaurant/Kustaurant/Utils/Enums/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  Kustaurant
+//
+//  Created by 송우진 on 7/23/24.
+//
+
+import Foundation
+
+enum Location: String {
+    case l1 = "건입~중문"
+    case l2 = "중문~어대"
+    case l3 = "후문"
+    case l4 = "정문"
+    case l5 = "구의역"
+}
