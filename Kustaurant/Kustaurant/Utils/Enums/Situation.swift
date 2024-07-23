06b782a2b0d78be0895f571d8fc67cdc4c329124
Woojin Song <woojin1900@gmail.com>
2024-07-23 20:34:09 +0900

    add Situation enum

---
+++ Kustaurant/Kustaurant/Utils/Enums/Situation.swift
@@ -0,0 +1,21 @@
+//
+//  Situation.swift
+//  Kustaurant
+//
+//  Created by 송우진 on 7/23/24.
+//
+
+import Foundation
+
+enum Situation: String {
+    case all = "전체"
+    case one = "혼밥"
+    case two = "2~4인"
+    case three = "5인 이상"
+    case four = "단체 회식"
+    case five = "배달"
+    case six = "야식"
+    case seven = "친구 초대"
+    case eight = "데이트"
+    case nine = "소개팅"
+}
