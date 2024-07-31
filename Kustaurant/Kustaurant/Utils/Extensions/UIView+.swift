5555f11c44d8f977390d3e303cfa28d245c759f8
yeonduing <yeonduing@gmail.com>
2024-07-31 18:07:04 +0900

    [UI] autoLayout 요소 추가

--- Kustaurant/Kustaurant/Utils/Extensions/UIView+.swift
+++ Kustaurant/Kustaurant/Utils/Extensions/UIView+.swift
@@ -26,9 +26,14 @@ extension UIView: Ku {
         
         case centerX(CGFloat)
         case centerY(CGFloat)
+        case center(CGFloat)
         
         case width(CGFloat)
         case height(CGFloat)
+        
+        case fillX(CGFloat)
+        case fillY(CGFloat)
+        case fill(CGFloat)
     }
 }
 
@@ -38,28 +43,47 @@ extension Ku where Self: UIView {
         view.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(view)
         
-        let layouts = autoLayout.map { layout in
+        let layouts = autoLayout.map({ layout in
             switch layout {
-            case .top(let constant): view.topAnchor.constraint(equalTo: self.topAnchor, constant: constant)
-            case .leading(let constant): view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: constant)
-            case .bottom(let constant): view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: constant)
-            case .trailing(let constant): view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: constant)
+            case .top(let constant): [view.topAnchor.constraint(equalTo: self.topAnchor, constant: constant)]
+            case .leading(let constant): [view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: constant)]
+            case .bottom(let constant): [view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: constant)]
+            case .trailing(let constant): [view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: constant)]
                 
-            case .topEqual(let to, let constant): view.topAnchor.constraint(equalTo: to.topAnchor, constant: constant)
-            case .leadingEqual(let to, let constant): view.leadingAnchor.constraint(equalTo: to.leadingAnchor, constant: constant)
-            case .bottomEqual(let to, let constant): view.bottomAnchor.constraint(equalTo: to.bottomAnchor, constant: constant)
-            case .trailingEqual(let to, let constant): view.trailingAnchor.constraint(equalTo: to.trailingAnchor, constant: constant)
+            case .topEqual(let to, let constant): [view.topAnchor.constraint(equalTo: to.topAnchor, constant: constant)]
+            case .leadingEqual(let to, let constant): [view.leadingAnchor.constraint(equalTo: to.leadingAnchor, constant: constant)]
+            case .bottomEqual(let to, let constant): [view.bottomAnchor.constraint(equalTo: to.bottomAnchor, constant: constant)]
+            case .trailingEqual(let to, let constant): [view.trailingAnchor.constraint(equalTo: to.trailingAnchor, constant: constant)]
                 
-            case .topNext(let to, let constant): self.topAnchor.constraint(equalTo: to.bottomAnchor, constant: constant)
-            case .leadingNext(let to, let constant): self.leadingAnchor.constraint(equalTo: to.trailingAnchor, constant: constant)
+            case .topNext(let to, let constant): [self.topAnchor.constraint(equalTo: to.bottomAnchor, constant: constant)]
+            case .leadingNext(let to, let constant): [self.leadingAnchor.constraint(equalTo: to.trailingAnchor, constant: constant)]
                 
-            case .centerX(let constant): view.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: constant)
-            case .centerY(let constant): view.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: constant)
+            case .centerX(let constant): [view.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: constant)]
+            case .centerY(let constant): [view.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: constant)]
+            case .center(let constant): [
+                view.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: constant),
+                view.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: constant)
+            ]
                 
-            case .width(let constant): view.widthAnchor.constraint(equalToConstant: constant)
-            case .height(let constant): view.heightAnchor.constraint(equalToConstant: constant)
+            case .width(let constant): [view.widthAnchor.constraint(equalToConstant: constant)]
+            case .height(let constant): [view.heightAnchor.constraint(equalToConstant: constant)]
+                
+            case .fillX(let constant): [
+                view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: constant),
+                view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: constant)
+            ]
+            case .fillY(let constant): [
+                view.topAnchor.constraint(equalTo: self.topAnchor, constant: constant),
+                view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: constant)
+            ]
+            case .fill(let constant): [
+                view.topAnchor.constraint(equalTo: self.topAnchor, constant: constant),
+                view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: constant),
+                view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: constant),
+                view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: constant)
+            ]
             }
-        }
+        }).flatMap { $0 }
         NSLayoutConstraint.activate(layouts)
     }
 }
