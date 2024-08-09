326904e2039312435e0690451326beb85a03496b
peppermint100 <krpeppermint100@gmail.com>
2024-08-09 13:28:09 +0900

    feat: NavigationBar

--- Kustaurant/Kustaurant/Presentation/Draw/DrawViewController.swift
+++ Kustaurant/Kustaurant/Presentation/Draw/DrawViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 final class DrawViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupNavigationBar()
+    }
+}
+
+extension DrawViewController {
+    
+    private func setupNavigationBar() {
+        let searchImage = UIImage(named: "icon_search")
+        let bellImage = UIImage(named: "icon_bell_badged")
+        let searchButtonView = UIImageView(image: searchImage)
+        let notificationButtonView = UIImageView(image: bellImage)
+        let searchButton = UIBarButtonItem(customView: searchButtonView)
+        let notificationButton = UIBarButtonItem(customView: notificationButtonView)
+        let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        space.width = 16.0
+        navigationItem.title = "랜덤 맛집 뽑기"
+        navigationItem.rightBarButtonItems = [searchButton, space, notificationButton]
     }
 }
