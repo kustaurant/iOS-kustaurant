f11d1f258a0c4630a656c542f86d5b8e8d4d1ff3
peppermint100 <krpeppermint100@gmail.com>
2024-08-21 13:11:55 +0900

    refactor: 로그인 관련 객체들 네이밍, 파일 위치 수정

---
+++ Kustaurant/Kustaurant/Data/Dto/AccessTokenResponse.swift
@@ -0,0 +1,12 @@
+//
+//  AccessTokenResponse.swift
+//  Kustaurant
+//
+//  Created by peppermint100 on 8/21/24.
+//
+
+import Foundation
+
+struct AccessTokenResponse: Codable {
+    let accessToken: String
+}
