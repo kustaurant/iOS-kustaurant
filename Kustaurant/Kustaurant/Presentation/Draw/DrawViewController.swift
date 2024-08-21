fbfb78924f1309f559c115699ea4c186d090b9d4
peppermint100 <krpeppermint100@gmail.com>
2024-08-21 10:39:49 +0900

    fix: 뽑기, 뽑기 결과화면 알림 버튼 제거

--- Kustaurant/Kustaurant/Presentation/Draw/DrawViewController.swift
+++ Kustaurant/Kustaurant/Presentation/Draw/DrawViewController.swift
@@ -42,15 +42,10 @@ extension DrawViewController {
     
     private func setupNavigationBar() {
         let searchImage = UIImage(named: "icon_search")
-        let bellImage = UIImage(named: "icon_bell_badged")
         let searchButtonView = UIImageView(image: searchImage)
-        let notificationButtonView = UIImageView(image: bellImage)
         let searchButton = UIBarButtonItem(customView: searchButtonView)
-        let notificationButton = UIBarButtonItem(customView: notificationButtonView)
-        let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        space.width = 16.0
         navigationItem.title = "랜덤 맛집 뽑기"
-        navigationItem.rightBarButtonItems = [searchButton, space, notificationButton]
+        navigationItem.rightBarButtonItems = [searchButton]
     }
     
     private func bind() {
