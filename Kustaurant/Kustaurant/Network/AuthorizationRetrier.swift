d7a97dbfb67a1cd7559a43a98a068b94d7a70109
peppermint100 <krpeppermint100@gmail.com>
2024-08-27 15:09:40 +0900

    feat: Refresh Token API와 retrier를 통해 리프레시 토큰을 요청 하여 세팅 후 API 재요청

---
+++ Kustaurant/Kustaurant/Network/AuthorizationRetrier.swift
@@ -0,0 +1,72 @@
+//
+//  AuthorizationRetrier.swift
+//  Kustaurant
+//
+//  Created by peppermint100 on 8/27/24.
+//
+
+import Foundation
+
+public class AuthorizationRetrier: RequestRetrier {
+    
+    private let interceptor: RequestInterceptor
+    private let networkService: NetworkService
+    
+    init(interceptor: RequestInterceptor, networkService: NetworkService) {
+        self.interceptor = interceptor
+        self.networkService = networkService
+    }
+    
+    public func shouldRetry(_ request: URLRequest, with error: Error, attempt: Int) -> Bool {
+        return false
+    }
+    
+    public func shouldRetryAsync(with error: Error, attempt: Int) async -> Bool {
+        if attempt > 2 {
+            return false
+        }
+        
+        guard
+            let error = error as? NetworkError,
+            (error == .unauthorized || error == .forbidden)
+        else {
+            return false
+        }
+        
+        return await refreshToken()
+    }
+    
+    private func refreshToken() async -> Bool {
+        guard let user: KuUser = KeychainStorage.shared.getValue(forKey: KeychainKey.kuUser) else {
+            return false
+        }
+        
+        let url = networkService.appConfiguration.apiBaseURL + "/api/v1/new-access-token"
+        let urlBuilder = URLRequestBuilder(url: url, method: .post)
+        let request = Request(session: URLSession.shared, interceptor: interceptor, retrier: nil)
+        let response = await request.responseAsync(with: urlBuilder)
+        
+        if let error = response.error {
+            /// refreshToken API에서 valid 한 토큰을 보내면 400에러를 내려주기 때문에 토큰 갱신 완료 처리
+            if error == .badRequest {
+                return false
+            }
+            
+            if error == .unauthorized || error == .forbidden {
+                return true
+            }
+            
+            Logger.error(error.localizedDescription, category: .network)
+            return false
+        }
+        
+        guard let data: AccessTokenResponse = response.decode() else {
+            Logger.error("Fail to Decoding Refresh Token API", category: .network)
+            return false
+        }
+        
+        let updatedUser = KuUser(id: user.id, accessToken: data.accessToken, provider: user.provider)
+        KeychainStorage.shared.setValue(updatedUser, forKey: KeychainKey.kuUser)
+        return true
+    }
+}
