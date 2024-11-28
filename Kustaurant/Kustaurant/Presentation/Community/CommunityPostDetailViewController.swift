2da40e536e9b5fdd8af0fe56d0541f19c5e7a7c0
Woojin Song <woojin1900@gmail.com>
2024-11-28 18:31:21 +0900

    chore: 댓글 작성 코드 주석 제거

--- Kustaurant/Kustaurant/Presentation/Community/CommunityPostDetailViewController.swift
+++ Kustaurant/Kustaurant/Presentation/Community/CommunityPostDetailViewController.swift
@@ -114,7 +114,6 @@ extension CommunityPostDetailViewController {
         let deleteAction = UIAction(title: "삭제하기", image: UIImage(named: "icon_trash"), attributes: .destructive) { [weak self] _ in
             self?.viewModel.process(.touchDeleteMenu)
         }
-        /*
         let writeCommentAction = UIAction(title: "댓글작성") { [weak self] _ in
             self?.viewModel.process(.touchWriteCommentMenu)
         }
@@ -122,10 +121,7 @@ extension CommunityPostDetailViewController {
         if viewModel.post.isPostMine ?? false {
             childrenActions.insert(deleteAction, at: 0)
         }
-         */
-        let childrenActions: [UIMenuElement] = [deleteAction]
         let menu = UIMenu(title: "", children: childrenActions)
-        menuEllipsisButton.isHidden = !(viewModel.post.isPostMine ?? false)
         menuEllipsisButton.menu = menu
         menuEllipsisButton.showsMenuAsPrimaryAction = true
     }
